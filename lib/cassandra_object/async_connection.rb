4f9034719dc1da2693cefa07916f8c35692f7d69
Doug Youch <doug@sessionm.com>
2012-03-28 14:50:45 -0400

    Added disconnect to AsyncConnection

--- lib/cassandra_object/async_connection.rb
+++ lib/cassandra_object/async_connection.rb
@@ -23,6 +23,12 @@ module CassandraObject
         end
         def self.connection?() !!connection end
 
+        def self.disconnect!
+          @@fiber_connections.delete(Fiber.current.object_id).tap { |conn|
+            conn.disconnect! if conn
+          }
+        end
+
         def connection
           defined?(@connection) ? @connection : singleton_class.connection
         end
