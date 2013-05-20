0494b5d134e1e7c9ae0631440e15cec49608de93
Doug Youch <doug@sessionm.com>
2013-05-19 22:53:06 -0400

    disable debug messages unless logger is in debug mode

--- lib/cassandra_object/log_subscriber.rb
+++ lib/cassandra_object/log_subscriber.rb
@@ -1,30 +1,35 @@
 module CassandraObject
   class LogSubscriber < ActiveSupport::LogSubscriber
     def add(event)
+      return unless logger && logger.debug?
       name = '%s add (%.1fms)' % [event.payload[:column_family], event.duration]
 
       debug "  #{name}  #{event.payload[:key]}[#{event.payload[:column]}]#{event.payload[:sub_column] ? '[' + event.payload[:sub_column] + ']' : ''} by #{event.payload[:value]}"
     end
 
     def get(event)
+      return unless logger && logger.debug?
       name = '%s get (%.1fms)' % [event.payload[:column_family], event.duration]
       
       debug "  #{name}  #{event.payload[:key]}"
     end
 
     def get_counter(event)
+      return unless logger && logger.debug?
       name = '%s get_counter (%.1fms)' % [event.payload[:column_family], event.duration]
 
       debug "  #{name}  #{event.payload[:key]}[#{event.payload[:column]}]"
     end
 
     def multi_get(event)
+      return unless logger && logger.debug?
       name = '%s multi_get (%.1fms)' % [event.payload[:column_family], event.duration]
 
       debug "  #{name}  (#{event.payload[:keys].size}) #{event.payload[:keys].join(" ")}"
     end
 
     def remove(event)
+      return unless logger && logger.debug?
       name = '%s remove (%.1fms)' % [event.payload[:column_family], event.duration]
 
       message = "  #{name}  #{event.payload[:key]}"
@@ -34,24 +39,28 @@ module CassandraObject
     end
 
     def truncate(event)
+      return unless logger && logger.debug?
       name = '%s truncate (%.1fms)' % [event.payload[:column_family], event.duration]
 
       debug "  #{name}  #{event.payload[:column_family]}"
     end
 
     def insert(event)
+      return unless logger && logger.debug?
       name = '%s insert (%.1fms)' % [event.payload[:column_family], event.duration]
 
       debug "  #{name}  #{event.payload[:key]} #{event.payload[:attributes].inspect}"
     end
 
     def get_range(event)
+      return unless logger && logger.debug?
       name = '%s get_range (%.1fms)' % [event.payload[:column_family], event.duration]
       
       debug "  #{name}  (#{event.payload[:count]}) '#{event.payload[:start]}' => '#{event.payload[:finish]}'"
     end
 
     def get_slice(event)
+      return unless logger && logger.debug?
       name = '%s get_slice (%.1fms)' % [event.payload[:column_family], event.duration]
       
       debug "  #{name}  #{event.payload[:key]} '#{event.payload[:start]}' => '#{event.payload[:finish]}'"
